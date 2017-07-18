c5b5569885398c88a8a27f98b5f7d3d46e943372
rjdeniega <ronald_deniega@dlsu.edu.ph>
2017-07-18 18:20:06 +0800

    something?

--- checkout_process/views.py
+++ checkout_process/views.py
@@ -29,10 +29,7 @@ class CartView(View):
             "line_items": line_items
         })
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 7331f88a759a9f3a49b22aea1e1d87e44a5f53a9
         return render(request, 'cart.html', context)
 
 # TODO: Checkout and Purchase -h
@@ -48,7 +45,4 @@ class PurchaseView(View):
     @customer_required
     def get(request):
         return render(request, 'purchase.html')
-<<<<<<< HEAD
 
-=======
->>>>>>> 7331f88a759a9f3a49b22aea1e1d87e44a5f53a9
