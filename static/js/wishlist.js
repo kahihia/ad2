adc0b3033784b42845f8433e16f228db8ba3dbb7
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2017-07-20 15:31:50 +0800

    Fixed JS

--- static/js/wishlist.js
+++ static/js/wishlist.js
@@ -1,19 +1,10 @@
 $(document).ready(function () {
-    $('.wish-button').on('click', function () {
-        if ($(this).parent().parent().parent().hasClass('wished')) {
-            $(this).parent().parent().parent().removeClass('wished').attr('title', 'Add to Wishlist');
-        }
-        else {
-            $(this).parent().parent().parent().addClass('wished').attr('title', 'Remove from Wishlist');
-        }
-    });
-
     $('.wish-link').on('click', function () {
-        if ($(this).parent().parent().hasClass('wished')) {
-            $(this).parent().parent().removeClass('wished').attr('title', 'Add to Wishlist');
+        if ($(this).hasClass('wished')) {
+            $(this).removeClass('wished').attr('title', 'Add to Wishlist');
         }
         else {
-            $(this).parent().parent().addClass('wished').attr('title', 'Remove from Wishlist');
+            $(this).addClass('wished').attr('title', 'Remove from Wishlist');
         }
     });
 });
