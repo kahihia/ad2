a72326180ef860e0f4be5837dcaba7a7e79ef373
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2017-07-20 15:14:35 +0800

    Wishlist link works

--- static/js/wishlist.js
+++ static/js/wishlist.js
@@ -7,4 +7,13 @@ $(document).ready(function () {
             $(this).parent().parent().parent().addClass('wished').attr('title', 'Remove from Wishlist');
         }
     });
+
+    $('.wish-link').on('click', function () {
+        if ($(this).parent().parent().hasClass('wished')) {
+            $(this).parent().parent().removeClass('wished').attr('title', 'Add to Wishlist');
+        }
+        else {
+            $(this).parent().parent().addClass('wished').attr('title', 'Remove from Wishlist');
+        }
+    });
 });
