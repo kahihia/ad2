cb445e900bf24e084810c32c51603996d1d7c029
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2017-07-20 15:10:39 +0800

    Fixed wishlist even more

---
+++ static/js/wishlist.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.wish-button').on('click', function () {
+        if ($(this).parent().parent().parent().hasClass('wished')) {
+            $(this).parent().parent().parent().removeClass('wished').attr('title', 'Add to Wishlist');
+        }
+        else {
+            $(this).parent().parent().parent().addClass('wished').attr('title', 'Remove from Wishlist');
+        }
+    });
+});
