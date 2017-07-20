cab5e0a170380b851071ec2f0579cc5782f55dcd
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2017-07-20 21:39:26 +0800

    Fixed date values in reports

---
+++ static/js/dates.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    var currentDate = new Date();
+    var month = currentDate.getMonth() + 1;
+    var day = currentDate.getDate();
+    var year = currentDate.getFullYear();
+
+    var string = "" + year;
+
+    if (month < 10)
+        string += "-0" + month;
+    else
+        string += "-" + month;
+
+    if (day < 10)
+        string += "-0" + day;
+    else
+        string += "-" + day;
+
+    $('.form-group > input').attr('value', string);
+    console.log(string);
+});
