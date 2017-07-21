562f78ef2e72ecff406ae3c2d99cf8cccb71f270
kamgamboa <kmllgmb@gmail.com>
2017-07-21 14:18:42 +0800

    Fixed wishlist.js

--- static/js/wishlist.js
+++ static/js/wishlist.js
@@ -1,3 +1,20 @@
+function attachCSRF() {
+    const csrftoken = $.cookie('csrftoken');
+
+    const csrfSafeMethod = function (method) {
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    };
+
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+
+}
+
 $(() => {
     $('.wish-link').on('click', function () {
         if ($(this).hasClass('wished')) {
@@ -5,13 +22,36 @@ $(() => {
             link.attr('title', 'Add this to my Wishlist');
             link.attr('data-original-title', 'Add this to my Wishlist');
             $('[data-toggle="tooltip"]').tooltip();
+
         }
         else {
             const link = $(this).addClass('wished');
             link.attr('title', 'Remove this from my Wishlist');
             link.attr('data-original-title', 'Remove this from my Wishlist');
+
+            attachCSRF()
+            $product_id = $(this).parent().find('.product-id-input').val();
+            console.log($product_id)
+
+            const dict = {
+                "product_id": $product_id
+            };
+
+            $.ajax({
+                url: window.location.pathname + "wishlist/",
+                method: "POST",
+                data: JSON.stringify(dict),
+                success: function () {
+                    alert("Product successfully added to your wishlist!")
+                },
+                error: function () {
+                    alert("yikes! something went wrong");
+                }
+
+            })
         }
     });
 
     $('[data-toggle="tooltip"]').tooltip();
 });
+
