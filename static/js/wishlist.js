66da4c956bd933df4292655dbf1d0fee889e87ec
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2017-07-21 14:25:15 +0800

    Fixed merge errors

--- static/js/wishlist.js
+++ static/js/wishlist.js
@@ -17,41 +17,6 @@ function attachCSRF() {
 
 $(() => {
     $('.wish-link').on('click', function () {
-<<<<<<< HEAD
-        if ($(this).hasClass('wished')) {
-            const link = $(this).removeClass('wished');
-            link.attr('title', 'Add this to my Wishlist');
-            link.attr('data-original-title', 'Add this to my Wishlist');
-            $('[data-toggle="tooltip"]').tooltip();
-
-        }
-        else {
-            const link = $(this).addClass('wished');
-            link.attr('title', 'Remove this from my Wishlist');
-            link.attr('data-original-title', 'Remove this from my Wishlist');
-
-            attachCSRF()
-            $product_id = $(this).parent().find('.product-id-input').val();
-            console.log($product_id)
-
-            const dict = {
-                "product_id": $product_id
-            };
-
-            $.ajax({
-                url: window.location.pathname + "wishlist/",
-                method: "POST",
-                data: JSON.stringify(dict),
-                success: function () {
-                    alert("Product successfully added to your wishlist!")
-                },
-                error: function () {
-                    alert("yikes! something went wrong");
-                }
-
-            })
-=======
-
         const wishLink = $(this);
 
         if (wishLink.hasClass('wished')) {
@@ -90,9 +55,6 @@ $(() => {
                     link.attr('data-original-title', 'Remove this from my Wishlist');
                 }
             });
-
-
->>>>>>> 39dbd9371648af39afee79b0a1065e31cb276e92
         }
     });
 
