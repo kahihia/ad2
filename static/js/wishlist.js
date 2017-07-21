86e434a3d2380526d8dca917ec4afa47c2a432d2
Keith Tan <skeithtan@gmail.com>
2017-07-21 14:14:54 +0800

    Updated Wishlist back end toggle

--- static/js/wishlist.js
+++ static/js/wishlist.js
@@ -1,15 +1,63 @@
+function attachCSRF() {
+    const csrftoken = $.cookie('csrftoken');
+
+    const csrfSafeMethod = function (method) {
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    };
+
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+
+}
+
 $(() => {
     $('.wish-link').on('click', function () {
-        if ($(this).hasClass('wished')) {
-            const link = $(this).removeClass('wished');
-            link.attr('title', 'Add this to my Wishlist');
-            link.attr('data-original-title', 'Add this to my Wishlist');
-            $('[data-toggle="tooltip"]').tooltip();
+
+        const wishLink = $(this);
+
+        if (wishLink.hasClass('wished')) {
+
+
+            const productID = $(wishLink.parent().find('.product-id-input')[0]).val();
+
+            attachCSRF();
+            //Perform ajax
+            $.ajax({
+                url: '/product-catalog/wish/' + productID + '/',
+                method: 'POST',
+                success: () => {
+                    alert("SUCCESS");
+                    //Perform visual changes
+                    const link = wishLink.removeClass('wished');
+                    link.attr('title', 'Add this to my Wishlist');
+                    link.attr('data-original-title', 'Add this to my Wishlist');
+                }
+            });
+
         }
         else {
-            const link = $(this).addClass('wished');
-            link.attr('title', 'Remove this from my Wishlist');
-            link.attr('data-original-title', 'Remove this from my Wishlist');
+
+            const productID = $(wishLink.parent().find('.product-id-input')[0]).val();
+
+            attachCSRF();
+            //Perform ajax
+            $.ajax({
+                url: '/product-catalog/wish/' + productID + '/',
+                method: 'POST',
+                success: () => {
+                    //Perform visual changes
+                    const link = wishLink.addClass('wished');
+                    link.attr('title', 'Remove this from my Wishlist');
+                    link.attr('data-original-title', 'Remove this from my Wishlist');
+                }
+            });
+
+
         }
     });
 
