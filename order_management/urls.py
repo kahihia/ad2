701835cd9bbf05992d219cb52059cbb6776e0e80
Keith Tan <skeithtan@gmail.com>
2017-07-26 15:20:38 +0800

    Added nasty accordion hack, Added "This order has been cancelled" alert,  Added "This order has been shipped" alert

--- order_management/urls.py
+++ order_management/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     url(r'^$', UserOrdersView.as_view()),
     url(r'^(?P<order_id>(\d+))/$', OrderView.as_view()),
+    url(r'^(?P<order_id>(\d+))/cancel/', CancelOrderView.as_view()),
     url(r'^waitlist/(?P<product_id>(\d+))/$', WaitlistView.as_view()),
     url(r'^waitlist/$', UserWaitlistView.as_view()),
 ]
