0165c917e87275eb7a22d01e96075b3d2a6c9eb0
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2017-07-28 22:47:55 +0800

    Modified sign-up

--- static/js/sign_up.js
+++ static/js/sign_up.js
@@ -1,7 +1,26 @@
 $(document).ready(function () {
-    $('#sign-in-button').on('click', function () {
+
+    // Disables the button if the passwords do not match.
+    $('#re-type').on('input', function () {
         if ($('#password').val() != $('#re-type').val()) {
-            alert('Passwords do not match!');
+            $(this).addClass('form-control-danger').removeClass('form-control-success');
+            $(this).parent().addClass("form-group has-danger").removeClass('has-success');
+        }
+        else if ($('#password').val() == $('#re-type').val()) {
+            $(this).removeClass('form-control-danger').addClass('form-control-success');
+            $(this).parent().removeClass("form-group has-danger").addClass('has-success');
         }
     });
+
+    $('input.form-control').on('input', function () {
+        if ($(this).val().length != 0) {
+            $(this).addClass('form-control-success');
+            $(this).parent().addClass("form-group has-success");
+        }
+        else {
+            $(this).removeClass('form-control-success');
+            $(this).parent().removeClass("form-group has-success");
+        }
+    });
+
 });
